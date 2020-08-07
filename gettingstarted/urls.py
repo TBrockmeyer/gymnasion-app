70ab8a571d114100b1986e978b9d664cb0401edc
t.brockmeyer <t.brockmeyer@criteo.com>
2020-08-07 16:08:32 +0200

    Corrected urls.py to contain reference to chat template

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,6 +17,6 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
-    path("chat/", hello.views.db, name="chat"),
+    path("chat/", hello.views.chat, name="chat"),
     path("admin/", admin.site.urls),
 ]
