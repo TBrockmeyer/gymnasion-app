c52959fd7bd802c7140859f50e360ad982545612
t.brockmeyer <t.brockmeyer@criteo.com>
2020-08-12 15:37:07 +0200

    Implemented Consumers and basic Chat Channel functionality from Channels tutorial, until page 20 top

---
+++ chat/routing.py
@@ -0,0 +1,8 @@
+# routing configuration for the chat app that has a route to the consumer (Channels docs tutorial)
+# chat/routing.py
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+]
--- routing.py
+++ routing.py
@@ -1,6 +1,20 @@
 # mysite/routing.py
-from channels.routing import ProtocolTypeRouter
+#from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack # (Channels docs tutorial, page 19)
+from channels.routing import ProtocolTypeRouter, URLRouter # (Channels docs tutorial, page 19)
+import chat.routing # (Channels docs tutorial, page 19)
+
+"""
+The root routing configuration below ('websocket') specifies that when a connection is made to the Channels development server, the
+ProtocolTypeRouter will first inspect the type of connection. If it is a WebSocket connection (ws:// or wss://),
+the connection will be given to the AuthMiddlewareStack.
+"""
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack( # (Channels docs tutorial, page 20)
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
